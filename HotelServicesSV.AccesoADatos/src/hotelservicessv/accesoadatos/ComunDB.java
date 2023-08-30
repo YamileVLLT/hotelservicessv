7fa769ed4aa66f65cd55e567a9c5c3c9be226686
Andrea Moran <bm644974@gmail.com>
2023-08-30 09:10:42 -0600

    cambio delogin

--- HotelServicesSV.AccesoADatos/src/hotelservicessv/accesoadatos/ComunDB.java
+++ HotelServicesSV.AccesoADatos/src/hotelservicessv/accesoadatos/ComunDB.java
@@ -11,8 +11,8 @@ public class ComunDB {
     static int TIPODB = TipoDB.SQLSERVER;
     
     static String connectionUrlSqlServer = "jdbc:sqlserver://localhost;"
-            + "database=CatalogoEmpresasDB;"
-            + "user=JavaUser;"
+            + "database=HotelServicesSV;"
+            + "user=hotel;"
             + "password=#Modulo16;"
             + "loginTimeout=30;encrypt=false;trustServerCertificate=false";
     
